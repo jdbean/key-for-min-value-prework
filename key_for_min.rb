026da95e22fcc1fa25e6801e62d27e5809c640fc
J.D. Bean <jonathandbean@gmail.com>
2018-02-18 19:18:30 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,8 +4,7 @@ require 'pry'
 
 def key_for_min_value(name_hash)
   smallest_name = nil
-  if name_hash.first != nil
-  smallest_value = name_hash.first[1]
+  if name_hash.first != nil { smallest_value = name_hash.first[1] }
   name_hash.each do |name, value|
     if value <= smallest_value
       smallest_value = value
